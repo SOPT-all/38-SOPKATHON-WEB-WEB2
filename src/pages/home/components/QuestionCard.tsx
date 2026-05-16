d529662508681f55a05815496efbb554a4f6d9f6
seojin15 <leogin3315@gmail.com>
2026-05-17 04:54:59 +0900

    fix: format 적용

--- src/pages/home/components/QuestionCard.tsx
+++ src/pages/home/components/QuestionCard.tsx
@@ -4,12 +4,14 @@ import { Chip, TextButton } from '@/shared/ui';
 
 type QuestionCardState = 'default' | 'complete';
 
-type QuestionCardProps = Omit<ComponentPropsWithoutRef<'article'>, 'children'> &
-  {
-    question: string;
-    state?: QuestionCardState;
-    onAnswerClick?: () => void;
-  };
+type QuestionCardProps = Omit<
+  ComponentPropsWithoutRef<'article'>,
+  'children'
+> & {
+  question: string;
+  state?: QuestionCardState;
+  onAnswerClick?: () => void;
+};
 
 export default function QuestionCard({
   question,
@@ -24,17 +26,17 @@ export default function QuestionCard({
       className='flex h-[17.4rem] w-[32.7rem] flex-col rounded-[1.6rem] bg-white p-[1.6rem]'
       {...props}
     >
-      <Chip variant="question">오늘의 질문</Chip>
+      <Chip variant='question'>오늘의 질문</Chip>
 
-      <p className="mt-[1.9rem] truncate text-neutral-900 typo-head-sb-18">
-        <span className="text-primary-500 mr-[0.2rem]">Q.</span>
+      <p className='typo-head-sb-18 mt-[1.9rem] truncate text-neutral-900'>
+        <span className='text-primary-500 mr-[0.2rem]'>Q.</span>
         {question}
       </p>
 
       <TextButton
-        className="mt-[2.7rem] w-full"
+        className='mt-[2.7rem] w-full'
         onClick={onAnswerClick}
-        size="small"
+        size='small'
         state={isComplete ? 'disabled' : 'default'}
       >
         {isComplete ? '답변완료' : '답변하기'}
