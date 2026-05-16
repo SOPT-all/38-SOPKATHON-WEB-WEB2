1e1892f28f18dd6c98cdee3c4d354fcb972e7388
hdg0116 <hdg0116@gmail.com>
2026-05-17 01:54:03 +0900

    feat: 바텀내비게이션 레이아웃 고정 및 화면 전환 기능 추가 (#21)

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,9 +1,14 @@
 import { Outlet } from 'react-router-dom';
 
+import BottomNavigation from '@/shared/ui/BottomNavigation';
+
 const Layout = () => {
   return (
     <>
-      <Outlet />
+      <main className="min-h-dvh pb-[8.3rem]">
+        <Outlet />
+      </main>
+      <BottomNavigation />
     </>
   );
 };
