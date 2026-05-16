d03a1860010c4217bcff42cca2e8d2abb5a21260
haeni <149465566+haeni82@users.noreply.github.com>
2026-05-17 05:52:12 +0900

    Feat: answer 페이지 구현 (#47)
    * feat: answer 페이지에 사용되는 chat box 구현
    
    * feat: answer 페이지 구현

--- src/pages/answer/answer.tsx
+++ src/pages/answer/answer.tsx
@@ -1,3 +1,5 @@
+import { QuestionBox } from '@/shared/ui';
+
 import ChatContainer from './components/chat-container';
 
 type UserRole = 'parent' | 'child';
@@ -6,9 +8,19 @@ const AnswerPage = () => {
   const role = (localStorage.getItem('role') ?? 'child') as UserRole;
 
   return (
-    <div className='flex flex-col gap-[1.6rem] px-[2rem] py-[2rem]'>
-      {/* 답변 전 예시 */}
-      <ChatContainer role={role} opponentHasAnswer={false} />
+    <div className='flex flex-col items-center gap-[2.4rem]'>
+      <div className='w-[32.7rem] pt-[2.4rem]'>
+        <QuestionBox
+          variant='today'
+          question='엄마가 좋아하는 음식이 뭐야?'
+          date={new Date()}
+          imageSrc=''
+          imageAlt='오늘의 질문 이미지'
+        />
+      </div>
+      <div className='w-[32.7rem] pb-[13.4rem]'>
+        <ChatContainer role={role} opponentHasAnswer={false} />
+      </div>
     </div>
   );
 };
