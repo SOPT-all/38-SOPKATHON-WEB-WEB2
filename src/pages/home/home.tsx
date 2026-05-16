3b72afe7e7d81c88f76485e756945eaa0a8c83ea
hdg0116 <hdg0116@gmail.com>
2026-05-17 04:47:42 +0900

    feat: 홈 화면 헤더 구현 (#38)

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,17 @@
+import HomeHeader from '@/pages/home/components/home-header';
+
 const HomePage = () => {
   return (
-    <div>
-      <h1>홈 - 오늘의 질문</h1>
-      {/* 오늘의 질문 */}
-      {/* 지난 답변 4개 */}
-      {/* 답변하러가기 버튼 */}
-    </div>
+    <>
+      <HomeHeader />
+
+      <main>
+        <h1>홈- 오늘의 질문</h1>
+        {/* 오늘의 질문 */}
+        {/* 지난 답변 4개 */}
+        {/* 답변하러가기 버튼 */}
+      </main>
+    </>
   );
 };
 
